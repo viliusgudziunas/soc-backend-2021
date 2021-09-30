f4462d983ecdd69dfba20327ddacf61ae9d8a5c2
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-09-30 21:17:51 +0300

    Trying to deploy: 1

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,40 @@
 import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import 'reflect-metadata';
-import { Connection, createConnection } from 'typeorm';
 import { AppRoutes } from './routes';
 
 dotenv.config();
+console.log('HTTP_PORT:', process.env.HTTP_PORT);
+console.log('PORT:', process.env.PORT);
+console.log('process.env:', process.env);
 
-const resolveConnectionByEnv = (): Promise<Connection> =>
-  process.env.ENV === 'production'
-    ? createConnection({ type: 'postgres', url: process.env.DATABASE_URL })
-    : createConnection();
+// const resolveConnectionByEnv = (): Promise<Connection> =>
+//   process.env.ENV === 'production'
+//     ? createConnection({ type: 'postgres', url: process.env.DATABASE_URL })
+//     : createConnection();
 
-resolveConnectionByEnv()
-  .then(async () => {
-    const app = express();
-    const port = 3000;
+// resolveConnectionByEnv()
+//   .then(async () => {
+const app = express();
+const port = 3000;
 
-    app.use(express.json());
+app.use(express.json());
 
-    AppRoutes.forEach((route) => {
-      const { action, method, path } = route;
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      app[method](path, (req: Request, res: Response, next: Function) => {
-        action(req, res)
-          .then(() => next)
-          .catch((error) => next(error));
-      });
-    });
+AppRoutes.forEach((route) => {
+  const { action, method, path } = route;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  app[method](path, (req: Request, res: Response, next: Function) => {
+    action(req, res)
+      .then(() => next)
+      .catch((error) => next(error));
+  });
+});
 
-    app.listen(port, () =>
-      // eslint-disable-next-line no-console
-      console.log(`SOC-backend is running on port: ${port}.`)
-    );
-  })
-  .catch((error) =>
-    console.error(`TypeOrm failed to connect to db \n${error}`)
-  );
+app.listen(port, () =>
+  // eslint-disable-next-line no-console
+  console.log(`SOC-backend is running on port: ${port}.`)
+);
+// })
+// .catch((error) =>
+//   console.error(`TypeOrm failed to connect to db \n${error}`)
+// );
