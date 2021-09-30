0598871ed73c37dd4b6973c1249a3933c2ee4c75
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-09-30 22:42:00 +0300

    Trying to deploy: 6

--- src/index.ts
+++ src/index.ts
@@ -21,28 +21,27 @@ const resolveConnectionByEnv = (): Promise<Connection> => {
   return createConnection();
 };
 
-resolveConnectionByEnv()
-  .then(async () => {
-    const app = express();
-    const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT;
 
-    app.use(express.json());
+app.use(express.json());
 
-    AppRoutes.forEach((route) => {
-      const { action, method, path } = route;
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      app[method](path, (req: Request, res: Response, next: Function) => {
-        action(req, res)
-          .then(() => next)
-          .catch((error) => next(error));
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`SOC-backend is running on port: ${port}.`);
+  resolveConnectionByEnv()
+    .then(async () => {
+      AppRoutes.forEach((route) => {
+        const { action, method, path } = route;
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        app[method](path, (req: Request, res: Response, next: Function) => {
+          action(req, res)
+            .then(() => next)
+            .catch((error) => next(error));
+        });
       });
-    });
-
-    app.listen(port, () =>
-      // eslint-disable-next-line no-console
-      console.log(`SOC-backend is running on port: ${port}.`)
+    })
+    .catch((error) =>
+      console.error(`TypeOrm failed to connect to db \n${error}`)
     );
-  })
-  .catch((error) =>
-    console.error(`TypeOrm failed to connect to db \n${error}`)
-  );
+});
